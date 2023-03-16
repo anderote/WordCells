6377191c99a5ccc4f979aa5cd73bb46d47814a9d
shira eisenberg <shiraeisenberg@uchicago.edu>
2023-03-16 15:30:35 -0400

    only addition

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def load_common_nouns(file_path):
         nouns = [line.strip() for line in f]
     return nouns
 
-glove_embeddings_path = 'glove.6B.50d.txt'  # Update this path to match the location of the downloaded file
+glove_embeddings_path = 'glove.6B.50d.txt'
 glove_model = load_glove_embeddings(glove_embeddings_path)
-common_nouns_path = 'common_nouns.txt'  # Update this path to match the location of the common_nouns.txt file
+common_nouns_path = 'common_nouns.txt'
 common_nouns = load_common_nouns(common_nouns_path)
 
 @app.route("/")
@@ -33,14 +33,10 @@ def index():
 @app.route("/generate_puzzle")
 def generate_puzzle():
     words = random.sample([word for word in common_nouns if word in glove_model], 2)
-    operator = random.choice(["+", "-"])
 
-    if operator == "+":
-        result_embedding = glove_model[words[0]] + glove_model[words[1]]
-    else:
-        result_embedding = glove_model[words[0]] - glove_model[words[1]]
+    result_embedding = glove_model[words[0]] + glove_model[words[1]]
 
-    return jsonify({"puzzle": f"{words[0]} {operator} {words[1]}", "result_embedding": result_embedding.tolist()})
+    return jsonify({"puzzle": f"{words[0]} + {words[1]}", "result_embedding": result_embedding.tolist()})
 
 @app.route("/calculate_similarity", methods=["POST"])
 def calculate_similarity():
