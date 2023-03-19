fc3ab88c18789c255ff73855e869f47096c1e0a1
Andrew Cote <andrew.cote00@gmail.com>
2023-03-19 01:08:04 -0700

    implemented midi music playing

--- app.py
+++ app.py
@@ -47,53 +47,37 @@ def load_glove_embeddings(file_path):
             embeddings[word] = vector
     return embeddings
 
-def load_common_nouns(file_path):
+def load_words(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
-        nouns = [line.strip() for line in f]
-    return nouns
+        words = [line.strip() for line in f]
+    return words
 
-def load_common_adjectives(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        adjectives = [line.strip() for line in f]
-    return adjectives
+def load_wordlists():
+    wordlists = {"synonyms": {}, "antonyms": {}}
+    for w_type in wordlists.keys():
+        with open(f"wordlists/{w_type}.txt", "r") as file:
+            for line in file:
+                word, answers = line.strip().split("|")
+                wordlists[w_type][word] = answers.split(",")
+    return wordlists
 
-def load_common_verbs(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        verbs = [line.strip() for line in f]
-    return verbs
 
 glove_embeddings_path = 'glove.6B.50d.txt'
-glove_model = load_glove_embeddings(glove_embeddings_path)
 common_nouns_path = 'wordlists/nouns.txt'
-common_nouns = load_common_nouns(common_nouns_path)
 common_adjectives_path = 'wordlists/adjectives.txt'
-common_adjectives = load_common_adjectives(common_adjectives_path)
+antonyms_path = 'wordlists/antonyms.txt'
+synonyms_path = 'wordlists/synonyms.txt'
 
+wordlists = load_wordlists()
+common_adjectives = load_words(common_adjectives_path)
+common_nouns = load_words(common_nouns_path)
+antonyms = load_words(antonyms_path)
+synonyms = load_words(synonyms_path)
+glove_model = load_glove_embeddings(glove_embeddings_path)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-def generate_grammatical_puzzle():
-    pattern = random.choice(["noun_adjective", "adjective_adjective", "noun_noun"])
-    
-    if pattern == "noun_adjective":
-        noun = random.choice([word for word in common_nouns if word in glove_model])
-        adjective = random.choice([word for word in common_adjectives if word in glove_model])
-        result_embedding = glove_model[noun] + glove_model[adjective]
-        puzzle = f"{noun} + {adjective}"
-    if pattern == "noun_noun":
-        noun_1 = random.choice([word for word in common_nouns if word in glove_model])
-        noun_2 = random.choice([word for word in common_nouns if word in glove_model])
-        result_embedding = glove_model[noun_1] + glove_model[noun_2]
-        puzzle = f"{noun_1} + {noun_2}"
-    else:  # pattern == "adjective_adjective"
-        adjective_1 = random.choice([word for word in common_adjectives if word in glove_model])
-        adjective_2 = random.choice([word for word in common_adjectives if word in glove_model])
-        result_embedding = glove_model[adjective_1] + glove_model[adjective_2]
-        puzzle = f"{adjective_1} + {adjective_2}"
-        
-    return puzzle, result_embedding
 
 # Add the following import
 from itertools import combinations
